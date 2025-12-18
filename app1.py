5333390f3994502c0fcdea48ea20a32769e9c121
Purva-D-Shah <purvashah2504@gmail.com>
2025-12-18 18:43:46 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -144,7 +144,7 @@ def process_data(orders_file, same_month_file, next_month_file, cost_file, packa
         df_pkg_final = pd.concat([df_pkg, total_row], ignore_index=True)
         
         # Write the new sheet
-        df_pkg_final.to_excel(writer, sheet_name='Actual cost(Delivered, Return & Exchange)', index=False)
+        df_pkg_final.to_excel(writer, sheet_name='Actual cost', index=False)
         # --------------------------------------------
 
         df_same_sheet.to_excel(writer, sheet_name='same month', index=False)
@@ -208,6 +208,7 @@ if orders_file and same_month_file and next_month_file and cost_file:
                     
                     st.download_button("⬇️ Download Excel Report", data=excel_data, file_name="Final_Report.xlsx", use_container_width=True, type="primary")
                 st.balloons()
+
 
 
 
